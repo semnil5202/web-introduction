75d78eb9101297fe8349fb669b819039d5ac3ff0
semnil5202 <semnil5202@gmail.com>
2023-02-17 02:39:14 +0900

    feat: 방문록 작성 기능 추가

--- js/App.js
+++ js/App.js
@@ -1,7 +1,40 @@
 export default class App {
-  alertMethod() {
-    alert('hello');
+  #nickname;
+  #checkedBoxes;
+  #guests;
+
+  readGuestState() {
+    const assignButton = document
+      .querySelector('#guest-item-container button[type="button"]')
+      .addEventListener('click', (event) => {
+        this.readInputsText(event);
+      });
+  }
+
+  readInputsText(event) {
+    this.#nickname = document.querySelector('#guest-item-input input');
+    this.#checkedBoxes = document.querySelectorAll(
+      '#guest-item-container div input[type="checkbox"]:checked + label > span',
+    );
+
+    this.renderGuests();
+  }
+
+  renderGuests() {
+    this.#guests = document.getElementById('guest-reply');
+    this.#guests.innerHTML += `
+    <div id="guest-reply-item">
+      <h3>${this.#nickname.value}</h3>
+      ${[...this.#checkedBoxes].map((movie) => `<span>${movie.innerHTML}</span>`)}
+    </div>`;
+
+    this.#nickname.value = '';
+    [...this.#checkedBoxes].forEach((span) => {
+      if (span.parentNode.previousElementSibling.checked) {
+        span.parentNode.previousElementSibling.checked = false;
+      }
+    });
   }
 }
 
-new App().alertMethod();
+new App().readGuestState();
