2d498bdad9e33595d6882edd51ec4f4be534cc60
semnil5202 <semnil5202@gmail.com>
2023-02-17 03:08:46 +0900

    feat: 스크롤 시 한 페이지씩 이동

--- js/App.js
+++ js/App.js
@@ -3,6 +3,48 @@ export default class App {
   #checkedBoxes;
   #guests;
 
+  scrollLock() {
+    window.onload = function () {
+      const articles = document.querySelectorAll('article');
+      const articleCount = articles.length;
+      articles.forEach(function (item, index) {
+        item.addEventListener('mousewheel', (event) => {
+          event.preventDefault();
+          let delta = 0;
+
+          if (!event) event = window.event;
+          if (event.wheelDelta) {
+            delta = event.wheelDelta / 120;
+            if (window.opera) delta = -delta;
+          } else if (event.detail) delta = -event.detail / 3;
+
+          let moveTop = window.scrollY;
+          let articleSelector = articles[index];
+
+          if (delta < 0) {
+            if (articleSelector !== articleCount - 1) {
+              moveTop =
+                window.pageYOffset + articleSelector.nextElementSibling.getBoundingClientRect().top;
+            }
+          } else {
+            if (articleSelector !== 0) {
+              moveTop =
+                window.pageYOffset +
+                articleSelector.previousElementSibling.getBoundingClientRect().top;
+            }
+          }
+
+          const body = document.querySelector('html');
+          window.scrollTo({ top: moveTop, left: 0, behavior: 'smooth' });
+        });
+      });
+    };
+
+    this.readGuestState();
+  }
+
+  scrollPosition(event) {}
+
   readGuestState() {
     const assignButton = document
       .querySelector('#guest-item-container button[type="button"]')
@@ -37,4 +79,4 @@ export default class App {
   }
 }
 
-new App().readGuestState();
+new App().scrollLock();
